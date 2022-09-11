2b617a2d4ddab1243821b59e7c26872c08f14161
AboutShaw <shawsi1988@gmail.com>
2022-09-11 12:50:00 +0100

    ProductTiles.vue refactored into vue format

--- products-fe/src/App.vue
+++ products-fe/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import ProductsList from './components/ProductsList.vue'
+
 </script>
 
 <template>
@@ -11,7 +12,7 @@ import TheWelcome from './components/TheWelcome.vue'
   </header>
 
   <main>
-    <TheWelcome />
+    <ProductsList />
   </main>
 </template>
 
---
+++ products-fe/src/components/ProductTiles.vue
@@ -0,0 +1,22 @@
+<script>
+  const convertToPounds = (priceValue).toFixed(2);
+
+  export default {
+    name: 'ProductTiles',
+    props: ['name', 'description', 'priceValue', 'weight', 'department', 'type']
+  }
+</script>
+
+<template>
+  <article>
+    <h3>{{name}}</h3>
+    <p>Price: £{{convertToPounds}}</p>
+    <p>{{description}}</p>
+    <p>Weight: {{weight}}g</p>
+    <p>Category: {{department}}: {{type}}</p>
+  </article>
+</template>
+
+<style>
+
+</style>
